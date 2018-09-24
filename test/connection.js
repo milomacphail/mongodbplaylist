83be3d8e96ccb085d6e31e51ed2f7dc690a21e9a
Milo MacPhail <milomacphail@gmail.com>
2018-09-24 22:37:23 +0000

    Fixed Authentication

--- test/connection.js
+++ test/connection.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 
+//ES6 Promises
+mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/testaroo', {useNewUrlParser: true});
+//Connect to the database before tests run
 
-mongoose.connection.once('open', function(){
+before(function(done){
+    mongoose.connect('mongodb://MiloMacPhail:Pandahead7!@ds153890.mlab.com:53890/mongodbplaylist', {useNewUrlParser: true});
+    mongoose.connection.once('open', function(){
    console.log("Connection made!!!");
-}).on('error', function(){
+   done();
+}).on('error', function(error){
     console.log('Connection error:', error);
 });
+});
+
